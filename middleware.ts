9bc8a514267ae573b91c329cb02038b6e550eaa0
david.chien <f312213213david@gmail.com>
2023-12-13 13:32:02 +0800

    fix: login

--- middleware.ts
+++ middleware.ts
@@ -49,5 +49,5 @@ export async function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/((?!api|_next/data|_next/static|_next/image|favicon.ico).*)",
+  matcher: ["/((?!api|favicon.ico|static|_next).*)"],
 };
