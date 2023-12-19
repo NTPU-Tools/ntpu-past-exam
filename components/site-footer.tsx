920ac3d21475afd68c20011c33b59ec8cda3c7e7
david.chien <f312213213david@gmail.com>
2023-12-19 17:07:24 +0800

    feat: adjust footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -3,7 +3,7 @@ function SiteFooter() {
     <footer className="py-3 md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-14 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Built by team:{" "}
+          Ship by{" "}
           <a
             href="https://github.com/NTPU-Past-Exam"
             target="_blank"
@@ -12,7 +12,7 @@ function SiteFooter() {
           >
             NTPU Tools
           </a>
-          . Made with ❤️
+          .
         </p>
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
           version:{" "}
