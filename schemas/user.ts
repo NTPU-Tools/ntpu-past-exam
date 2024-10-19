dfd8c330fe0c9ca71f236de1f7a285e06d523a6f
david.chien <f312213213david@gmail.com>
2024-10-20 01:58:10 +0800

    PE-18 fix: note should be optional

--- schemas/user.ts
+++ schemas/user.ts
@@ -3,5 +3,5 @@ import * as z from "zod";
 export const editUserInfoSchema = z.object({
   name: z.string({ required_error: "請輸入姓名" }).min(1, "請輸入姓名"),
   major: z.string({ required_error: "請選擇主修" }).min(1, "請選擇主修"),
-  note: z.string().optional(),
+  note: z.string().nullable(),
 });
